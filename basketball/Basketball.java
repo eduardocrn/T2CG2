9e915f9594d38f81b0585733c0d5708015764c28
eduardocrn <eduardo-curcino@hotmail.com>
2015-11-22 14:11:28 -0200

    merge

--- basketball/Basketball.java
+++ basketball/Basketball.java
@@ -3,8 +3,13 @@ package basketball;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.awt.TextRenderer;
+
+import engine.BasketConfiguration;
+import engine.BasketEngine;
 import net.sourceforge.*;
 
-public class Basketball {
+public class Basketball implements BasketConfiguration{
+	
+	BasketEngine engine = new BasketEngine();
 	
 }
